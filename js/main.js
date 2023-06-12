c1f30efafad3cdf9da14d8d01af761a967605561
Caroline Faria <caroline.gdfaria@gmail.com>
2023-06-12 13:16:38 -0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,36 @@
+const MAIN = document.getElementById('content')
+let db
+
+fetch('./data.json')
+.then(response => {return response.json()})
+.then(data => {
+  db = data.comments
+  criaSessao()
+})
+.catch(error => {
+  // Trate qualquer erro que ocorrer durante a requisição
+  console.log('Ocorreu um erro:', error);
+});
+
+function criaSessao() {
+  db.forEach(el => {
+    const SESSAO = `
+      <section>
+        <header>
+          <img src='${el.user.image.png}'>
+          <span class='username'>${el.user.username}</span>
+          <span class='createdAt'>${el.createdAt}</span>
+          <span class='reply_field'>
+            <svg width="14" height="13" xmlns="http://www.w3.org/2000/svg"><path d="M.227 4.316 5.04.16a.657.657 0 0 1 1.085.497v2.189c4.392.05 7.875.93 7.875 5.093 0 1.68-1.082 3.344-2.279 4.214-.373.272-.905-.07-.767-.51 1.24-3.964-.588-5.017-4.829-5.078v2.404c0 .566-.664.86-1.085.496L.227 5.31a.657.657 0 0 1 0-.993Z" fill="#5357B6"/></svg>
+            Reply
+          </span>
+        </header>
+        <main>
+          <p class='comment'>${el.content}</p>
+        </main>
+      </section>
+    `
+    MAIN.insertAdjacentHTML('afterbegin', SESSAO)
+    console.log(el)
+  })
+}
