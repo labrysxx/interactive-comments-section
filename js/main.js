d690e25c054bcbe71c93b94a0512d27763dbee4e
Caroline Faria <caroline.gdfaria@gmail.com>
2023-06-13 11:25:54 -0300

    alinhando comentários

--- js/main.js
+++ js/main.js
@@ -16,7 +16,7 @@ fetch('./data.json')
 function criaSessao() {
   db.forEach(el => {
     const SESSAO = `
-      <section>
+      <section class='main-comment'>
         <div class='up-down-vote'>
           <button class="upvote-btn">+</button>
           <span class="score">${el.score}</span>
@@ -35,31 +35,39 @@ function criaSessao() {
           <p class='comment'>${el.content}</p>
         </main>
       </section>
-      <div class='respostas'></div>
     `
     MAIN.insertAdjacentHTML('afterbegin', SESSAO)
 
-    if(el.replies.length !== 0) {
-      console.log(el.replies)
-      const RESPOSTAS_FIELD = document.querySelectorAll('.respostas')
-  
+    if (el.replies && el.replies.length > 0) {
       el.replies.forEach((resposta) => {
-        for(let j = 0; j < RESPOSTAS_FIELD.length; j++) {
-          const RESPOSTA_DIV = document.createElement('div')
-          RESPOSTA_DIV.classList.add('resposta')
-          RESPOSTA_DIV.textContent = resposta.content
-          RESPOSTAS_FIELD[j].appendChild(RESPOSTA_DIV)
-        }
-      })
+        const respostaElemento = `
+          <section class='sub-comment'>
+            <div class='up-down-vote'>
+              <button class="upvote-btn">+</button>
+              <span class="score">${resposta.score}</span>
+              <button class="downvote-btn">-</button>
+            </div>
+            <header class='cabecalho'>
+              <img src='${resposta.user.image.png}'>
+              <span class='username'>${resposta.user.username}</span>
+              <span class='createdAt'>${resposta.createdAt}</span>
+              <span class='reply_field'>
+                <svg width="14" height="13" xmlns="http://www.w3.org/2000/svg"><path d="M.227 4.316 5.04.16a.657.657 0 0 1 1.085.497v2.189c4.392.05 7.875.93 7.875 5.093 0 1.68-1.082 3.344-2.279 4.214-.373.272-.905-.07-.767-.51 1.24-3.964-.588-5.017-4.829-5.078v2.404c0 .566-.664.86-1.085.496L.227 5.31a.657.657 0 0 1 0-.993Z" fill="#5357B6"/></svg>
+                Reply
+              </span>
+            </header>
+            <main class='main-content'>
+              <p class='comment'>${resposta.content}</p>
+            </main>
+          </section>
+        `;
+        const mainComment = document.querySelector('.main-comment');
+        mainComment.insertAdjacentHTML('afterend', respostaElemento);
+      });
     }
-
   })
 }
 
 function ordenaDb(a, b) {
   return a.score - b.score
-}
-
-function adicionaComentario() {
-
 }
