6d9e36be2983636e74c66378e5f4ec249840f6ce
Caroline Faria <caroline.gdfaria@gmail.com>
2023-06-14 10:44:56 -0300

    adiciona sessão para comentar abaixo dos comentários

--- js/loadComment.js
+++ js/loadComment.js
@@ -1,17 +1,17 @@
-const COMMENT = document.querySelector('textarea')
-let comentarios = Array()
+const text = document.querySelector('textarea')
+let comments = Array()
 
 const FORM = document.querySelector('form').addEventListener('submit', (e) => {
   e.preventDefault()
-  adicionaComentario()
-  limpaForm()
-  carregaComentario()
+  addComment()
+  cleanComments()
+  loadComment()
 })
 
-class Comentario {
+class Comment {
   static idCounter = 1
   constructor(content, createdAt, score, user, replies, image) {
-    this.id = Comentario.idCounter++
+    this.id = Comment.idCounter++
     this.content = content;
     this.createdAt = createdAt;
     this.score = score;
@@ -21,68 +21,105 @@ class Comentario {
   }
 }
 
-function adicionaComentario() {
-  if(COMMENT.value.length === 0) {
+function addComment() {
+  if(text.value.length === 0) {
     return alert('Digite seu comentário')
   } else {
-    const NOVO_COMENTARIO = new Comentario(COMMENT.value, 'now', 0, 'juliusomo', [], "./images/avatars/image-juliusomo.png")
-    comentarios.push(NOVO_COMENTARIO)
+    const new_comment = new Comment(text.value, 'now', 0, 'juliusomo', [], "./images/avatars/image-juliusomo.png")
+    comments.push(new_comment)
 
-    atualizaLocalStorage()
+    updateLocalStorage()
   }
 }
 
-function atualizaLocalStorage() {
-  localStorage.setItem('comentarios', JSON.stringify(comentarios))
+function updateLocalStorage() {
+  localStorage.setItem('comments', JSON.stringify(comments))
 }
 
-function limpaForm() {
-  COMMENT.value = ''
+function cleanComments() {
+  text.value = ''
   const your_comment = document.querySelectorAll('.your-comment')
   for(let i = 0; i < your_comment.length; i++) {
     your_comment[i].remove()
   }
 }
 
-function checaLocalStorage() {
-  if(localStorage.getItem('comentarios')) {
-    comentarios = JSON.parse(localStorage.getItem('comentarios'))
+function checkLocalStorage() {
+  if(localStorage.getItem('comments')) {
+    comments = JSON.parse(localStorage.getItem('comments'))
   }
 }
 
-function carregaComentario() {
-  checaLocalStorage()
-  comentarios.forEach((comentario) => {
+function loadComment() {
+  checkLocalStorage()
+  comments.forEach((comment) => {
     const htmlComment = `
-      <section class='main-comment your-comment'>
+      <section class='main-comment your-comment comment'>
         <div class='up-down-vote'>
           <button class="upvote-btn">+</button>
-          <span class="score">${comentario.score}</span>
+          <span class="score">${comment.score}</span>
           <button class="downvote-btn">-</button>
         </div>
         <header class='cabecalho'>
-          <img src='${comentario.image}'>
-          <span class='username'>${comentario.user}</span>
-          <span class='createdAt'>${comentario.createdAt}</span>
-          <span class='reply_field' data-name='${comentario.user}'>
+          <div>
+            <img src='${comment.image}'>
+            <span class='username'>${comment.user}</span>
+            <span class='createdAt'>${comment.createdAt}</span>
+          </div>
+          <span class='reply_field' data-name='${comment.user}'>
             <svg width="14" height="13" xmlns="http://www.w3.org/2000/svg"><path d="M.227 4.316 5.04.16a.657.657 0 0 1 1.085.497v2.189c4.392.05 7.875.93 7.875 5.093 0 1.68-1.082 3.344-2.279 4.214-.373.272-.905-.07-.767-.51 1.24-3.964-.588-5.017-4.829-5.078v2.404c0 .566-.664.86-1.085.496L.227 5.31a.657.657 0 0 1 0-.993Z" fill="#5357B6"/></svg>
             Reply
           </span>
         </header>
         <main class='main-content'>
-          <p class='comment'>${comentario.content}</p>
+          <p class='comment'>${comment.content}</p>
         </main>
       </section>
     `;
-    MAIN.insertAdjacentHTML("beforeend", htmlComment);
+    main.insertAdjacentHTML("beforeend", htmlComment);
   })
 }
 
-carregaComentario()
+loadComment()
 
-const REPLY = document.querySelectorAll('.reply_field')
-for(let i = 0; i < REPLY.length; i++) {
-  REPLY[i].addEventListener('click', (e) => {
-    console.log(e.currentTarget.dataset.name)
-  })
+function addReplyFieldClickListeners() {
+  const reply = document.querySelectorAll('.reply_field');
+
+  for (let i = 0; i < reply.length; i++) {
+    reply[i].addEventListener('click', (e) => {
+      console.log(e)
+      const parentSection = e.currentTarget.closest('.comment');
+      let newSectionComment = parentSection.nextElementSibling;
+
+      if (newSectionComment && newSectionComment.classList.contains('new-comment')) {
+        // A nova seção já existe, então remova-a
+        newSectionComment.remove();
+      } else {
+        newSectionComment = document.createElement('section');
+        const textarea = document.createElement('textarea');
+        const btn = document.createElement('button');
+        newSectionComment.classList.add('new-comment');
+        btn.classList.add('btn-new-comment');
+        btn.innerHTML = 'SEND';
+
+        newSectionComment.appendChild(textarea);
+        newSectionComment.appendChild(btn);
+
+        // Inserir a nova seção como um novo filho do elemento pai
+        parentSection.insertAdjacentElement('afterend', newSectionComment);
+      }
+    });
+  }
+}
+
+addReplyFieldClickListeners()
+
+function sendAnswer() {
+  console.log(btn_resposta)
+
+  for(let i = 0; i < btn_resposta.length; i++) {
+    btn_resposta[i].addEventListener('click', () => {
+      console.log('enviou')
+    })
+  }
 }
--- js/main.js
+++ js/main.js
@@ -1,31 +1,33 @@
-const MAIN = document.getElementById('content')
+const main = document.getElementById('content')
 let db
 
 fetch('./data.json')
 .then(response => {return response.json()})
 .then(data => {
   db = data.comments
-  criaSessao()
+  createSection()
 })
 .catch(error => {
   // Trate qualquer erro que ocorrer durante a requisição
   console.log('Ocorreu um erro:', error);
 });
 
-function criaSessao() {
+function createSection() {
   db.sort(ordenaDb)
   db.forEach(el => {
-    const SESSAO = `
-      <section class='main-comment'>
+    const section = `
+      <section class='main-comment comment'>
         <div class='up-down-vote'>
           <button class="upvote-btn">+</button>
           <span class="score">${el.score}</span>
           <button class="downvote-btn">-</button>
         </div>
         <header class='cabecalho'>
-          <img src='${el.user.image.png}'>
-          <span class='username'>${el.user.username}</span>
-          <span class='createdAt'>${el.createdAt}</span>
+          <div>
+            <img src='${el.user.image.png}'>
+            <span class='username'>${el.user.username}</span>
+            <span class='createdAt'>${el.createdAt}</span>
+          </div>
           <span class='reply_field' data-name='${el.user.username}'>
             <svg width="14" height="13" xmlns="http://www.w3.org/2000/svg"><path d="M.227 4.316 5.04.16a.657.657 0 0 1 1.085.497v2.189c4.392.05 7.875.93 7.875 5.093 0 1.68-1.082 3.344-2.279 4.214-.373.272-.905-.07-.767-.51 1.24-3.964-.588-5.017-4.829-5.078v2.404c0 .566-.664.86-1.085.496L.227 5.31a.657.657 0 0 1 0-.993Z" fill="#5357B6"/></svg>
             Reply
@@ -36,33 +38,35 @@ function criaSessao() {
         </main>
       </section>
     `
-    MAIN.insertAdjacentHTML('afterbegin', SESSAO)
+    main.insertAdjacentHTML('afterbegin', section)
 
     if (el.replies && el.replies.length > 0) {
-      el.replies.forEach((resposta) => {
-        const respostaElemento = `
-          <section class='sub-comment'>
+      el.replies.forEach((answer) => {
+        const answerElement = `
+          <section class='sub-comment comment'>
             <div class='up-down-vote'>
               <button class="upvote-btn">+</button>
-              <span class="score">${resposta.score}</span>
+              <span class="score">${answer.score}</span>
               <button class="downvote-btn">-</button>
             </div>
             <header class='cabecalho'>
-              <img src='${resposta.user.image.png}'>
-              <span class='username'>${resposta.user.username}</span>
-              <span class='createdAt'>${resposta.createdAt}</span>
-              <span class='reply_field' data-name='${resposta.user.username}'>
+              <div>
+                <img src='${answer.user.image.png}'>
+                <span class='username'>${answer.user.username}</span>
+                <span class='createdAt'>${answer.createdAt}</span>
+              </div>
+              <span class='reply_field' data-name='${answer.user.username}'>
                 <svg width="14" height="13" xmlns="http://www.w3.org/2000/svg"><path d="M.227 4.316 5.04.16a.657.657 0 0 1 1.085.497v2.189c4.392.05 7.875.93 7.875 5.093 0 1.68-1.082 3.344-2.279 4.214-.373.272-.905-.07-.767-.51 1.24-3.964-.588-5.017-4.829-5.078v2.404c0 .566-.664.86-1.085.496L.227 5.31a.657.657 0 0 1 0-.993Z" fill="#5357B6"/></svg>
                 Reply
               </span>
             </header>
             <main class='main-content'>
-              <p class='comment'>${resposta.content}</p>
+              <p class='comment'>${answer.content}</p>
             </main>
           </section>
         `;
         const mainComment = document.querySelector('.main-comment');
-        mainComment.insertAdjacentHTML('afterend', respostaElemento);
+        mainComment.insertAdjacentHTML('afterend', answerElement);
       });
     }
   })
