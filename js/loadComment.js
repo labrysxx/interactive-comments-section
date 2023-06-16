eb6e8bbe5416cd16024c92ce34aa4130c784480b
Caroline Faria <caroline.gdfaria@gmail.com>
2023-06-16 12:46:51 -0300

    corrige bug da caixa de resposta

--- js/loadComment.js
+++ js/loadComment.js
@@ -3,7 +3,6 @@ const text = document.querySelector('textarea');
 const formComment = document.querySelector('#make-comment')
 
 let comments = Array();
-let replyButtons
 let replyingTo
 
 formComment.addEventListener('submit', (e) => {
@@ -66,78 +65,29 @@ function loadComment() {
   comments.forEach((comment) => {
     const htmlComment = `
     <section class='main-comment your-comment comment'>
-    <div class='up-down-vote'>
-    <button class="upvote-btn">+</button>
-    <span class="score">${comment.score}</span>
-          <button class="downvote-btn">-</button>
-          </div>
-        <header class='cabecalho'>
-          <div>
-            <img src='${comment.image}'>
-            <span class='username'>${comment.user}</span>
-            <span class='createdAt'>${comment.createdAt}</span>
-            </div>
-          <span data-name='${comment.user}'>  
-          <button class="edit-button">Editar</button>
-            <button class="delete-button">Excluir</button>
-          </span>
-          </header>
+      <div class='up-down-vote'>
+        <button class="upvote-btn">+</button>
+        <span class="score">${comment.score}</span>
+        <button class="downvote-btn">-</button>
+      </div>
+      <header class='cabecalho'>
+        <div>
+          <img src='${comment.image}'>
+          <span class='username'>${comment.user}</span>
+          <span class='createdAt'>${comment.createdAt}</span>
+        </div>
+        <span data-name='${comment.user}'>  
+          <button class="edit-button">Edit</button>
+          <button class="delete-button">Delete</button>
+        </span>
+      </header>
         <main class='main-content'>
           <p class='comment'>${comment.content}</p>
         </main>
-      </section>
+    </section>
     `;
     main.insertAdjacentHTML("beforeend", htmlComment);
   })
 }
 
-function addReplyFieldClickListeners() {
-  replyButtons = document.querySelectorAll('.reply_field');
-  console.log(replyButtons)
-  replyButtons.forEach((button) => {
-    button.addEventListener('click', () => {
-      const parentComment = closest(button, '.comment');
-      let replySection = parentComment.nextElementSibling;
-
-      // salva o nome da pessoa a ser respondida
-      replyingTo = parentComment.children[1].children[1].dataset.name
-
-      if (replySection && replySection.classList.contains('reply_section')) {
-        // A seção de resposta já existe, então remova-a
-        replySection.remove();
-      } else {
-        // cria a caixa de resposta
-        replySection = document.createElement('section');
-        const form = document.createElement('form');
-        const image = document.createElement('img');
-        const textarea = document.createElement('textarea');
-        const submitButton = document.createElement('input');
-
-        image.src = './images/avatars/image-juliusomo.png';
-
-        form.classList.add('make-answer');
-        replySection.classList.add('reply_section');
-        textarea.placeholder = 'Digite a resposta...';
-        submitButton.value = 'REPLY';
-        submitButton.type = 'submit';
-        textarea.value = `@${replyingTo} `
-
-        // igualar a largura da caiza de resposta à largura do comentário a ser respondido
-        if (parentComment.classList.contains('sub-comment')) {
-          replySection.classList.add('sub-comment');
-        }
-        
-        form.appendChild(image);
-        form.appendChild(textarea);
-        form.appendChild(submitButton);
-        replySection.appendChild(form);
-
-        // Inserir a nova seção abaixo do comentário
-        parentComment.insertAdjacentElement('afterend', replySection);
-      }
-    });
-  });
-}
-
 loadComment()
-addReplyFieldClickListeners()
