d54eee4fdeb53ea56260465207cc4aea788b9a86
Caroline Faria <caroline.gdfaria@gmail.com>
2023-06-18 22:11:49 -0300

    persistindo respostas na tela

--- js/main.js
+++ js/main.js
@@ -1,24 +1,90 @@
+const default_data = {
+  "comments": [
+    {
+      "id": 1,
+      "content": "Impressive! Though it seems the drag feature could be improved. But overall it looks incredible. You've nailed the design and the responsiveness at various breakpoints works really well.",
+      "createdAt": "1 month ago",
+      "score": 12,
+      "user": "amyrobson",
+      "image": "./images/avatars/image-amyrobson.png",
+      "replies": []
+    },
+    {
+      "id": 2,
+      "content": "Woah, your project looks awesome! How long have you been coding for? I'm still new, but think I want to dive into React as well soon. Perhaps you can give me an insight on where I can learn React? Thanks!",
+      "createdAt": "2 weeks ago",
+      "score": 5,
+      "user": "maxblagun",
+      "image": "./images/avatars/image-maxblagun.png", 
+      "replies": [
+        {
+          "id": 3,
+          "content": "If you're still new, I'd recommend focusing on the fundamentals of HTML, CSS, and JS before considering React. It's very tempting to jump ahead but lay a solid foundation first.",
+          "createdAt": "1 week ago",
+          "score": 4,
+          "replyingTo": "maxblagun",
+          "user": "ramsesmiron",
+          "image": "./images/avatars/image-ramsesmiron.png",
+          "replies": []
+        },
+        {
+          "id": 4,
+          "content": "I couldn't agree more with this. Everything moves so fast and it always seems like everyone knows the newest library/framework. But the fundamentals are what stay constant.",
+          "createdAt": "2 days ago",
+          "score": 2,
+          "replyingTo": "ramsesmiron",
+          "user": "juliusomo",
+          "image": "./images/avatars/image-juliusomo.png"
+        }
+      ]
+    }
+  ]
+}
+
 const main = document.getElementById('content')
-let db
 let comments = Array();
+let reply_btns
 
 window.addEventListener('DOMContentLoaded', () => {
-  fetch('./data.json')
-  .then(response => {return response.json()})
-  .then(data => {
-    db = data.comments
-    comments = db
-    console.log(comments)
-    createComment()
-    
-    const reply_btns = document.querySelectorAll('.reply_field')
-    addReplyFieldClickListeners(reply_btns)
-  })
-  .catch(error => {
-    // Trate qualquer erro que ocorrer durante a requisição
-    console.log('Ocorreu um erro:', error);
-  });
+  createComment()
 })
+
+function createComment() {
+  checkLocalStorage()
+  main.innerHTML = ''
+  comments.sort(ordenaDb)
+  comments.forEach((comment) => {
+    const section = `
+      <section class='main-comment comment'>
+        <div class='up-down-vote'>
+          <button class="upvote-btn">+</button>
+          <span class="score">${comment.score}</span>
+          <button class="downvote-btn">-</button>
+        </div>
+        <header class='cabecalho'>
+          <div>
+            <img src='${comment.image}'>
+            <span class='username'>${comment.user}</span>
+            <span class='createdAt'>${comment.createdAt}</span>
+          </div>
+          <span class='reply_field' data-name='${comment.user}'>
+            <svg width="14" height="13" xmlns="http://www.w3.org/2000/svg"><path d="M.227 4.316 5.04.16a.657.657 0 0 1 1.085.497v2.189c4.392.05 7.875.93 7.875 5.093 0 1.68-1.082 3.344-2.279 4.214-.373.272-.905-.07-.767-.51 1.24-3.964-.588-5.017-4.829-5.078v2.404c0 .566-.664.86-1.085.496L.227 5.31a.657.657 0 0 1 0-.993Z" fill="#5357B6"/></svg>
+            Reply
+          </span>
+        </header>
+        <main class='main-content'>
+          <p class='comment'>${comment.content}</p>
+        </main>
+      </section>
+    `
+    main.insertAdjacentHTML('afterbegin', section)
+    if (comment.replies && comment.replies.length > 0) {
+      createAnswer(comment)
+    }
+    addReplyFieldClickListeners()
+
+  })
+}
 
 class Comment {
   constructor(content, createdAt, replyingTo, score, user, replies, image) {
@@ -32,13 +98,6 @@ class Comment {
   }
 }
 
-function addComment(text) {
-  const new_comment = new Comment(text, new Date(), 'everybody', 0, 'juliusomo', [], "./images/avatars/image-juliusomo.png");
-  comments.push(new_comment);
-
-  updateLocalStorage();
-}
-
 function updateLocalStorage() {
   localStorage.setItem('comments', JSON.stringify(comments))
 }
@@ -46,52 +105,13 @@ function updateLocalStorage() {
 function checkLocalStorage() {
   if(localStorage.getItem('comments')) {
     comments = JSON.parse(localStorage.getItem('comments'))
-  }
-}
-
-// function cleanComments() {
-//   text.value = ''
-//   const your_comment = document.querySelectorAll('.your-comment')
-//   for(let i = 0; i < your_comment.length; i++) {
-//     your_comment[i].remove()
-//   }
-// }
-
-function createComment() {
-  checkLocalStorage()
-  comments.sort(ordenaDb)
-  for(let i = 0; i < comments.length; i++) {
-    const section = `
-      <section class='main-comment comment'>
-        <div class='up-down-vote'>
-          <button class="upvote-btn">+</button>
-          <span class="score">${comments[i].score}</span>
-          <button class="downvote-btn">-</button>
-        </div>
-        <header class='cabecalho'>
-          <div>
-            <img src='${comments[i].image}'>
-            <span class='username'>${comments[i].user}</span>
-            <span class='createdAt'>${comments[i].createdAt}</span>
-          </div>
-          <span class='reply_field' data-name='${comments[i].user}'>
-            <svg width="14" height="13" xmlns="http://www.w3.org/2000/svg"><path d="M.227 4.316 5.04.16a.657.657 0 0 1 1.085.497v2.189c4.392.05 7.875.93 7.875 5.093 0 1.68-1.082 3.344-2.279 4.214-.373.272-.905-.07-.767-.51 1.24-3.964-.588-5.017-4.829-5.078v2.404c0 .566-.664.86-1.085.496L.227 5.31a.657.657 0 0 1 0-.993Z" fill="#5357B6"/></svg>
-            Reply
-          </span>
-        </header>
-        <main class='main-content'>
-          <p class='comment'>${comments[i].content}</p>
-        </main>
-      </section>
-    `
-    main.insertAdjacentHTML('afterbegin', section)
-    if (comments[i].replies && comments[i].replies.length > 0) {
-      createAnswer(comments[i])
-    }
+  } else {
+    comments = default_data.comments
   }
 }
 
 function createAnswer(el) {
+  checkLocalStorage()
   el.replies.forEach((answer) => {
     const answerElement = `
       <section class='sub-comment comment'>
@@ -125,63 +145,86 @@ function ordenaDb(a, b) {
   return a.score - b.score
 }
 
-function addReplyFieldClickListeners(reply_btns) {
-  console.log(reply_btns)
+function addReplyFieldClickListeners() {
+  reply_btns = document.querySelectorAll('.reply_field');
   reply_btns.forEach((button) => {
-    button.addEventListener('click', () => {
-      const parentComment = button.closest('.comment');
-      let replySection = parentComment.nextElementSibling;
-       // salva o nome da pessoa a ser respondida
-      replyingTo = parentComment.children[1].children[1].dataset.name
-       if (replySection && replySection.classList.contains('reply_section')) {
-        // A seção de resposta já existe, então remova-a
-        replySection.remove();
-      } else {
-        // cria a caixa de resposta
-        replySection = document.createElement('section');
-        const form = document.createElement('form');
-        const image = document.createElement('img');
-        const textarea = document.createElement('textarea');
-        const submitButton = document.createElement('input');
-
-        image.src = './images/avatars/image-juliusomo.png';
-        textarea.classList.add('text-content')
-        form.classList.add('make-answer');
-        replySection.classList.add('reply_section');
-        textarea.placeholder = 'Digite a resposta...';
-        submitButton.classList.add('send-btn')
-        submitButton.value = 'REPLY';
-        submitButton.type = 'submit';
-        textarea.value = `@${replyingTo} `
-
-        // igualar a largura da caiza de resposta à largura do comentário a ser respondido
-        if (parentComment.classList.contains('sub-comment')) {
-          replySection.classList.add('sub-comment');
-        }
-        
-        form.appendChild(image);
-        form.appendChild(textarea);
-        form.appendChild(submitButton);
-        replySection.appendChild(form);
-
-        // Inserir a nova seção abaixo do comentário
-        parentComment.insertAdjacentElement('afterend', replySection);
-      }
-      const forms = document.querySelectorAll('.make-answer')
-      sendAnswer(forms)
-    });
+    button.removeEventListener('click', replyFieldClickHandler);
+    button.addEventListener('click', replyFieldClickHandler);
   });
 }
 
-function sendAnswer(data) {
-  for(let i = 0; i < data.length; i++) {
-    data[i].addEventListener('submit', (e) => {
-      e.preventDefault()
-      const content = e.target.children[1].value
-      console.log(content, data[i], data)
-      addComment(content)
-      main.innerHTML = ''
-      createComment()
-    })
+function replyFieldClickHandler() {
+  const parentComment = this.closest('.comment');
+  let replySection = parentComment.nextElementSibling;
+   // salva o nome da pessoa a ser respondida
+  replyingTo = parentComment.children[1].children[1].dataset.name
+   if (replySection && replySection.classList.contains('reply_section')) {
+    // A seção de resposta já existe, então remova-a
+    replySection.remove();
+  } else {
+    // cria a caixa de resposta
+    replySection = document.createElement('section');
+    const form = document.createElement('form');
+    const image = document.createElement('img');
+    const textarea = document.createElement('textarea');
+    const submitButton = document.createElement('input');
+  
+    image.src = './images/avatars/image-juliusomo.png';
+    textarea.classList.add('text-content')
+    form.classList.add('make-answer');
+    replySection.classList.add('reply_section');
+    textarea.placeholder = 'Type your answer...';
+    submitButton.classList.add('send-btn')
+    submitButton.value = 'REPLY';
+    submitButton.type = 'submit';
+  
+    // igualar a largura da caixa de resposta à largura do comentário a ser respondido
+    if (parentComment.classList.contains('sub-comment')) {
+      replySection.classList.add('sub-comment');
+    }
+    
+    form.appendChild(image);
+    form.appendChild(textarea);
+    form.appendChild(submitButton);
+    replySection.appendChild(form);
+  
+    // Inserir a nova seção abaixo do comentário
+    parentComment.insertAdjacentElement('afterend', replySection);
+    sendAnswer();
+  }
+}
+
+function sendAnswer() {
+  const forms = document.querySelectorAll('.make-answer')
+  for (let i = 0; i < forms.length; i++) {
+    forms[i].addEventListener('submit', (e) => {
+      e.preventDefault();
+      if(e.target.children[1].value.length === 0) {
+        alert('Answer field is empty, type something!')
+        return
+      }
+      const content = e.target.children[1].value;
+
+      // Encontre o comentário ao qual estamos respondendo
+      const replyingTo = e.target.parentNode.previousElementSibling.children[1].children[1].dataset.name;
+      const parentComment = comments.find(comment => comment.user === replyingTo);
+      console.log(comments.find(comment => comment.user === replyingTo))
+      if (parentComment) {
+        // Crie um novo objeto Comment para a resposta
+        const newAnswer = new Comment(content, new Date(), parentComment.user, 0, 'juliusomo', [], "./images/avatars/image-juliusomo.png");
+
+        // Adicione a resposta ao array de replies do comentário pai
+        parentComment.replies.push(newAnswer);
+
+        // Atualize o armazenamento local
+        updateLocalStorage();
+
+        // Crie novamente os comentários na tela, incluindo as respostas atualizadas
+        createComment(comments);
+      }
+
+      // Limpe o campo de resposta
+      e.target.children[1].value = '';
+    });
   }
 }
